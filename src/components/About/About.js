5c1d9da1fb28d1965ecc2e7f01d1453f5186cbaf
jingkang97 <jingkang@lomotif.com>
2021-09-09 14:41:25 +0800

    feat: aos heading

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,16 +3,34 @@ import Reveal from 'react-reveal/Reveal';
 import Wobble from 'react-reveal/Wobble';
 import Jump from 'react-reveal/Jump';
 import LightSpeed from 'react-reveal/LightSpeed';
+import Fade from 'react-reveal/Fade';
+
+import './styles.css'
 
 class About extends React.Component {
   render() {
     return (
         <div style={{background:'#1D1D1D', color:'white', height:'100vh', padding:'70px'}}>
-        <div style={{marginLeft:'20px', fontSize:'50px'}}>
+        <div style={{marginLeft:'20px', fontSize:'30px', color:'#25F4EE'}}>
         <LightSpeed left>
-            <Jump><h1 class="heading">About Me</h1></Jump>
-            
+            <Jump>
+                <h1>
+                    <div class="heading">A</div>
+                    <div class="heading">b</div>
+                    <div class="heading">o</div>
+                    <div class="heading">u</div>
+                    <div class="heading">t</div>
+                    &nbsp;
+                    <div class="heading">M</div>
+                    <div class="heading">e</div>
+                </h1>
+            </Jump>
         </LightSpeed>
+        <div style={{fontSize:'20px', color:'white'}}>
+            <Fade up>
+                My name is Jing Kang.
+            </Fade>
+        </div>
         </div>
         </div>
     );
