ab818b28c8e5b4de464a9c9e5c56270a7b11fc01
jingkang97 <jingkang@lomotif.com>
2021-09-09 15:20:42 +0800

    feat: about me para

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,21 +14,28 @@ class About extends React.Component {
         <div style={{marginLeft:'20px', fontSize:'30px', color:'#25F4EE'}}>
         <LightSpeed left>
             <Jump>
-                <h1>
-                    <div class="heading">A</div>
-                    <div class="heading">b</div>
-                    <div class="heading">o</div>
-                    <div class="heading">u</div>
-                    <div class="heading">t</div>
-                    &nbsp;
-                    <div class="heading">M</div>
-                    <div class="heading">e</div>
-                </h1>
+                <div style={{display:'flex', flexDirection:'row', alignItems:'center', height:'50px'}}>
+                    <h1 >
+                        <div class="heading">A</div>
+                        <div class="heading">b</div>
+                        <div class="heading">o</div>
+                        <div class="heading">u</div>
+                        <div class="heading">t</div>
+                        &nbsp;
+                        <div class="heading">M</div>
+                        <div class="heading">e</div>
+                        <span><img class="heading" src="jingkang.png" style={{height:'50px', marginBottom:'-10px', marginLeft:'10px'}}/></span>
+                    </h1>
+                </div>
+                
             </Jump>
         </LightSpeed>
         <div style={{fontSize:'20px', color:'white'}}>
             <Fade up>
-                My name is Jing Kang.
+                <p style={{lineHeight:'30px', letterSpacing: '2px'}}>Hello! My name is Jing Kang and I'm a final year Computer engineering student at the National University of Singapore.
+                    I am currently working at Lomotif as a part time web developer intern and I'm working towards 
+                    becoming a full stack web developer. 
+                </p>
             </Fade>
         </div>
         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,43 @@
-const Project = () => {
-    return ( 
-        <div style={{background:'black', color:'white', height:'600px', height:'100vh'}}>
-            Projects
+import React from 'react';
+import Reveal from 'react-reveal/Reveal';
+import Wobble from 'react-reveal/Wobble';
+import Jump from 'react-reveal/Jump';
+import LightSpeed from 'react-reveal/LightSpeed';
+import Fade from 'react-reveal/Fade';
+
+import './styles.css'
+
+class Project extends React.Component {
+  render() {
+    return (
+        <div style={{background:'#1D1D1D', color:'white', height:'100vh', padding:'70px'}}>
+        <div style={{marginLeft:'20px', fontSize:'30px', color:'#25F4EE'}}>
+        <LightSpeed left>
+            <Jump>
+                <h1>
+                    <div class="heading">A</div>
+                    <div class="heading">b</div>
+                    <div class="heading">o</div>
+                    <div class="heading">u</div>
+                    <div class="heading">t</div>
+                    &nbsp;
+                    <div class="heading">M</div>
+                    <div class="heading">e</div>
+                </h1>
+            </Jump>
+        </LightSpeed>
+        <div style={{fontSize:'20px', color:'white'}}>
+            <Fade up>
+                <p style={{lineHeight:'30px', letterSpacing: '2px'}}>Hello! My name is Jing Kang and I'm a final year Computer engineering student at the National University of Singapore.
+                    I am currently working at Lomotif as a part time web developer intern and I'm working towards 
+                    becoming a full stack web developer. 
+                </p>
+            </Fade>
         </div>
-     );
+        </div>
+        </div>
+    );
+  }
 }
- 
 export default Project;
+
